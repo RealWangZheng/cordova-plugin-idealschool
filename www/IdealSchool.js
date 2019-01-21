a92e283cbde70e7a2f70d6a6dc4584a6e3ed8813
RealWangZheng <realwangzheng@hotmail.com>
2019-01-21 10:46:45 +0800

    update js

--- www/IdealSchool.js
+++ www/IdealSchool.js
@@ -1,5 +1,65 @@
-var exec = require('cordova/exec');
-
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'IdealSchool', 'coolMethod', [arg0]);
-};
+cordova.define("cordova-plugin-idealschool.IdealSchool",     
+	function(require, exports, module) {         
+		var exec = require("cordova/exec");         
+		module.exports = {             
+			setToolbarTitle: function(title){                 
+				exec(                 
+					function (msg){},
+					function (msg){}, 
+					"IdealSchool", 
+					"setToolbarTitle", 
+					[title] );             
+			},             
+			setToolbarCloseButtonShow:function(show){                 
+				exec(                 
+					function (msg){}, 
+					function (msg){}, 
+					"IdealSchool",                 
+					"setToolbarCloseButtonShow",                 
+					[show]                 
+					);             
+			},             
+			setToolbarRightMenus:function(successCallback, errorCallback,menuItems){                 
+				exec(         
+					successCallback, 
+					errorCallback,                 
+					"IdealSchool",                 
+					"setToolbarRightMenus",                 
+					[menuItems]                 
+					);             
+			},             
+			selectContacts:function(successCallback, errorCallback,isMulti,title){                 
+				exec(                 
+					successCallback, 
+					errorCallback,                 
+					"IdealSchool",                 
+					"selectContacts",                 
+					[isMulti,title]                 
+					);             
+			},             
+			startLocation:function (successCallback,errorCallback) {                 
+				exec(                 successCallback,errorCallback,                 
+					'IdealSchool',                 
+					'startLocation',
+					[]
+					);             
+			},             
+			stopLocation:function (successCallback,errorCallback) {                 
+				exec(                 successCallback,errorCallback,                 
+					'IdealSchool',                 
+					'stopLocation',
+					[]
+					);             
+			},             
+			getUserInfo:function (successCallback,errorCallback) {                 
+				exec(                 
+					successCallback,
+					errorCallback,                 
+					'IdealSchool',                 
+					'getUserInfo',
+					[]
+					);             
+			}         
+		} 
+	}
+);
